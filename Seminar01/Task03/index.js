f127865530c013cf929b8a3630147282fb150d80
Valerii_Panov <val250640@mail.ru>
2024-04-23 17:41:30 +1000

    sem01

--- Seminar01/Task03/index.js
+++ Seminar01/Task03/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const articles = [{
+let articles = [{
     title: "Title 1",
     content: "Content article 1"
 },
@@ -11,44 +11,68 @@ const articles = [{
 
 const containerEl = document.querySelector('.container');
 
-const addFrame = (event, article) => {
+const createForm = () => {
     const formEl = document.createElement('form');
+    formEl.classList.add('add-form');
 
     const titleEl = document.createElement('input');
     titleEl.type = 'text';
-    if (!article) {
-        titleEl.value = 'New Article';
-    } else {
-        titleEl.value = article.title;
-    }
+    titleEl.classList.add('input-title');
     formEl.appendChild(titleEl);
 
     const contentEl = document.createElement('textarea');
-    if (!article) {
-        contentEl.placeholder = 'Enter your article content here...';
-    } else {
-        contentEl.value = article.content;
-    }
+    contentEl.classList.add('input-content');
     formEl.appendChild(contentEl);
 
+    formEl.style.display = 'none';
     containerEl.prepend(formEl);
+}
 
-    event.target.onclick = (event) => {
-        event.target.onclick = addFrame;
-        containerEl.removeChild(formEl);
+const addForm = (event, article) => {
+    const formEl = document.querySelector('.add-form');
+    const titleInput = formEl.querySelector('.input-title');
+    const contentInput = formEl.querySelector('.input-content');
+    const addButton = document.querySelector('.add-button');
+
+    if (!article) {
+        titleInput.value = 'New Article';
+        contentInput.placeholder = 'Enter your article content here...';
+    } else {
+        titleInput.value = article.title;
+        contentInput.value = article.content;
+    }
+
+    formEl.style.display = 'block';
+    addButton.onclick = (e) => {
+        formEl.style.display = 'none';
+        
         if (!article) {
-            addArticle({ title: titleEl.value, content: contentEl.value });
+            addArticle({ title: titleInput.value, content: contentInput.value });
+            articles.push({ title: titleInput.value, content: contentInput.value });
         } else {
-
+            const articleBox = event.target.parentElement.parentElement;
+            articleBox.querySelector('.title').innerText = titleInput.value;
+            articleBox.querySelector('.content').innerText = contentInput.value;
+            const findArticle = articles.filter(art => art === article)[0];
+            findArticle.title = titleInput.value;
+            findArticle.content = contentInput.value;
         }
+        e.target.onclick = addForm;
+
+        titleInput.value = 'New Article';
+        contentInput.value = '';
+
+        localStorage.setItem('articles', JSON.stringify(articles));
     };
 }
 
-const addButton = document.createElement('button');
-addButton.classList.add('add-button');
-addButton.textContent = 'Add article';
-addButton.onclick = addFrame;
-containerEl.appendChild(addButton);
+const createAddButton = () => {
+    const addButton = document.createElement('button');
+    addButton.classList.add('add-button');
+    addButton.textContent = 'Add article';
+    addButton.onclick = addForm;
+    containerEl.appendChild(addButton);
+}
 
 const addArticle = (article) => {
     const articleEl = document.createElement('div');
@@ -56,11 +80,11 @@ const addArticle = (article) => {
 
     const titleEl = document.createElement('h2');
     titleEl.innerText = article.title;
-    articleEl.classList.add('title');
+    titleEl.classList.add('title');
     articleEl.appendChild(titleEl);
     const contentEl = document.createElement('p');
     contentEl.innerText = article.content;
-    articleEl.classList.add('content');
+    contentEl.classList.add('content');
     articleEl.appendChild(contentEl);
 
     const buttonsBox = document.createElement('div');
@@ -69,19 +93,22 @@ const addArticle = (article) => {
     delButton.textContent = 'Delete';
     delButton.addEventListener('click', () => {
         containerEl.removeChild(articleEl);
+        articles = articles.filter(art => art !== article);
     });
     buttonsBox.appendChild(delButton);
     const editButton = document.createElement('button');
     editButton.classList.add('edit-button');
     editButton.textContent = 'Edit';
     editButton.addEventListener('click', () => {
-        addFrame(this.Event, article);
+        addForm(this.event, article);
     });
     buttonsBox.appendChild(editButton);
 
     articleEl.appendChild(buttonsBox);
     containerEl.appendChild(articleEl);
-    articles.push({ title: titleEl.value, content: contentEl.value });
 }
 
+createAddButton();
+createForm();
+articles = JSON.parse(localStorage.getItem('articles'));
 articles.forEach(article => addArticle(article));
